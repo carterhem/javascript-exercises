0c0507501d910a1fbb8400c1e346b04478e66661
Carter <carter.hemington@gmail.com>
2023-01-13 13:46:40 -0500

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfTimes) {
+  let endString = '';
+  if(numOfTimes < 0) {
+    endString = 'ERROR'
+  } else {
+    for(let i = 1; i <= numOfTimes; i++) {
+      endString += string;
+    }
+  }
+  
+  return endString
 };
 
 // Do not edit below this line
