f10ef2d0748cdbf4a00f4d7dfd8a6011dc7caa09
Carter <carter.hemington@gmail.com>
2023-01-13 14:53:10 -0500

    complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  if(year % 4 == 0 && year % 100 != 0 || year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+    //if year divided by 4 AND year is not divisible by 100 then TRUE
+    //if year divided by 4 AND year is divisible by 100 AND year is also divisible by 400 then TRUE
+    return true;
+  } else {
+    return false;
+    //if the above conditions are not met, return false
+  }
 
 };
 
