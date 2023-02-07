ad6c69064189cce6af63085837316ab4acb26a80
Carter <carter.hemington@gmail.com>
2023-02-07 12:17:30 -0500

    start exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+// create string/ lowercase it, remove all spaces and punctiation
+let cleanedString = input.toLowerCase()
+// create a reverse copy of the string
+//compare cleaned string to cleaned reverse string
+//if cleaned string and cleaned reverse string are the same, then return true
 };
 
 // Do not edit below this line
