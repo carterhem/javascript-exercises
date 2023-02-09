9d7845b90f18fe0289e86390edf9dded1c041ddc
Carter <carter.hemington@gmail.com>
2023-02-09 11:44:51 -0500

    complete exercise 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+  // array.forEach(element => console.log("element.yearOfBirth", element.yearOfBirth));
+  array.forEach(element => {
+    //looping through each element
+    if(element.yearOfDeath) {
+      //if year of death exists, use it
+      let age = element.yearOfDeath - element.yearOfBirth;
+      //define age
+      element.age = age;
+      //add age to element
+    } else if (!element.yearOfDeath) {
+      //if yearOfDeath doesn't exists
+      let age = new Date().getFullYear() - element.yearOfBirth
+      //use current date to find age
+      element.age = age;
+      //add age to element
+    }
+  })
+  const newArray = array.sort((a,b) => a.age > b.age ? -1 : 1)
+  //create newArray from original array sorting based on age (oldest first)
+  return newArray[0];
+  //return first index of newArray
+}; 
 
 // Do not edit below this line
 module.exports = findTheOldest;
